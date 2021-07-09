e0afac86bfc458b2d0231059c2143c6f3c83436d
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2021-07-09 10:59:09 +0300

    Print stuff to see how data looks like from at

--- Backend/USSD/v1/routes.py
+++ Backend/USSD/v1/routes.py
@@ -5,10 +5,14 @@ ussd = Blueprint("ussd",__name__)
 
 @ussd.route("/launch", methods=['GET', 'POST'])
 def ussd_launch():
-    sessiond_id = request.values.get("sessionId", None)
+    session_id = request.values.get("sessionId", None)
     service_code = request.values.get("serviceCode", None)
     phone_number = request.values.get("phoneNumber", None)
     text = request.values.get("text", "default")
+    print(session_id)
+    print(service_code)
+    print(phone_number)
+    print(text)
 
     if text == '':
         response = "CON Welcome to Badili.co Move money anywhere.\n"
