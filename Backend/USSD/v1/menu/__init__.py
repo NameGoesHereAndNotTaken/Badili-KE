5e2e95405fbfa5aae15b7a24b48eaa6a38dada88
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2021-07-13 18:14:45 +0300

    Add formatted code to see how it will peform

---
+++ Backend/USSD/v1/menu/__init__.py
@@ -0,0 +1,101 @@
+import re
+from Backend.models import User
+class Register:
+    def __init__(self, menu_items):
+        self.menu_items = menu_items
+        self.user = self.is_user_registered()
+        self.determine_level()
+        self.response = None
+    
+    def determine_level(self):
+        if len(self.menu_items) == 1:
+            self.level_one()
+        
+        if len(self.menu_items) == 2:
+            self.level_two()
+
+        if len(self.menu_items) == 3:
+            self.level_three()
+
+        if len(self.menu_items) == 4:
+            self.level_four()
+
+    def level_one(self):
+        if self.menu_items[0] == '':
+            if self.user:
+                response = f"CON Welcome back {self.user.first_name} \n"
+                response += "1. Send Money \n"
+                response += "2. Withdraw Money \n"
+                response += "3. Top up Account \n"
+                self.response = response
+            else:
+                response = "CON Welcome to Badili. The future of mobile money\n"
+                response += "1. Register Account"
+                self.response = response
+        else:
+            if not self.user:                
+                response = "CON Enter your National ID number"
+                self.response = response
+
+    def level_two(self):
+        response = "CON Enter pin to secure your account"
+        self.response = response
+
+    def level_three(self):
+        response = "CON Confirm pin"
+        self.response = response
+
+    def level_four(self):
+        print(self.menu_items)
+        pass
+
+    def get_response(self):
+        return self.response
+
+    def get_text(self):
+        return self.text
+
+    @classmethod
+    def is_user_registered(cls, phone_number):
+        return User.check_user_exists(phone_number)
+    
+class SendMoney:
+    def __init__(self, menu_items):
+        self.menu_items = menu_items
+        self.determine_level()
+
+    def determine_level(self):
+        if len(self.menu_items) == 1:
+            self.level_one()
+        elif len(self.menu_items) == 2:
+            self.level_two()
+    
+    def level_one(self):
+        response = "CON 1. Badili Account\n"
+        response += "2. Stellar Address"
+        self.response = response
+
+    def level_two(self):
+        if self.menu_items[1] == "1":
+            response = "CON Enter Account Number of receipient"
+        else:
+            response = "CON Enter Stellar Address of receipient"
+            
+        self.response = response
+
+    def level_three(self):
+        pass
+
+    def get_response(self):
+        return self.response
+class FundAccount:
+    pass
+
+class Withdraw:
+    pass
+
+class Account:
+    pass
+
+class Settings:
+    pass
--- Backend/USSD/v1/routes.py
+++ Backend/USSD/v1/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from Backend.models import User
 from Backend import psql
+from .menu import Register, SendMoney
 
 ussd = Blueprint("ussd",__name__)
 
@@ -9,29 +10,37 @@ def ussd_launch():
     session_id = request.values.get("sessionId", None)
     service_code = request.values.get("serviceCode", None)
     phone_number = request.values.get("phoneNumber", None)
-    text = request.values.get("text", "default")
-    
-    if text == '':
-        #TODO: Check if user exists 
-        user = User.check_user_exists(phone_number)
-        if user:
-            response = f"CON Welcome back {user.first_name} \n"
-            response += "1. Send Money \n"
-            response += "2. Withdraw Money \n"
-            response += "3. Top up Account \n"
-            text = '2'
+    text = request.values.get("text", "")
+    menu_items = get_menu_items(text)
+
+    if menu_items[0] == '':
+        register  = Register(menu_items)
+        response = register.get_response()
+
+    elif menu_items[0] == "1":
+        user = Register.is_user_registered(phone_number)
+        if not user:
+            register  = Register(menu_items)
+            response = register.get_response()
+
         else:
-            response = "CON Welcome to Badili. The future of mobile money\n"
-            response += "1. Register Account"
-        
-    elif text == '1':
-        response = "CON Enter your National ID number"
-        # user = User("00000001", phone_number, "6341")
-        # if user:
-        #     print("User is correct")
-        #     print(user)
-        #     psql.session.add(user)
-        #     psql.session.commit()
-    
-    
+            send_money = SendMoney(menu_items)
+            response = send_money.get_response()
+
+    elif
     return response
+
+def get_menu_items(text):
+    return text.split('*')
+
+def is_first_entry(menu_items):
+    return True if len(menu_items) == 1 and menu_items[0] == '' else False
+
+def is_second_entry(menu_items):
+    return True if len(menu_items) == 2 else False
+
+def register_account_part_one(menu_items):
+    return True if menu_items[0] == "1" and menu_items[1] != '' else False
+
+def register_account_part_two(menu_items):
+    return True if menu_items[0] == "1" and menu_items[1] != '' and menu_items[2] != '' else False
