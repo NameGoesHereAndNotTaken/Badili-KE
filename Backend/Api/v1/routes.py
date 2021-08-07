8ea810ffd852998f50be4bc73451121017e3493e
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2021-08-07 18:47:48 +0300

    Reformat spi signin response

--- Backend/Api/v1/routes.py
+++ Backend/Api/v1/routes.py
@@ -40,7 +40,7 @@ def api_signin():
     if user:
         if user.verify_password(password):
             token = user.encode_auth_token(user.id)
-            response = {'status': 'success', 'message':'User signin successful', 'auth_token': token, 'user': {"email": email, "names": user.first_name +" "+ user.last_name} }
+            response = {'status': 'success', 'message':'User signin successful', 'user': {"email": email, "names": user.first_name +" "+ user.last_name, 'stellar_address':user.public_key, 'auth_token': token} }
             return make_response(jsonify(response)), 200
         else:
             response = {'status': 'fail', 'message':'Email or password mismatch'}
